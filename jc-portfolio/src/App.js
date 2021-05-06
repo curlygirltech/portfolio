0c46093ee5c56fa6eeef4c87ab3a87602368ebe0
jcameron10 <jovonnec@gmail.com>
2021-05-06 18:47:16 -0400

    created routes

--- jc-portfolio/src/App.js
+++ jc-portfolio/src/App.js
@@ -3,11 +3,19 @@ import "./App.css";
 import About from "./Screens/About";
 import { Route, Switch } from "react-router";
 import Home from "./Screens/Home";
+import Contact from "./Screens/Contact";
+import Projects from "./Screens/Projects";
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path='/projects'>
+          <Projects />
+        </Route>
+        <Route path= '/contact'>
+          <Contact/>
+        </Route>
         <Route path="/about">
           <About />
         </Route>
