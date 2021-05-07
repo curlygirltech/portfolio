99f40d59045e015e635a43e4a90b64a3945c31da
jcameron10 <jovonnec@gmail.com>
2021-05-07 08:18:38 -0400

    added pic and imported fonts

--- jc-portfolio/src/Screens/Home.jsx
+++ jc-portfolio/src/Screens/Home.jsx
@@ -6,7 +6,9 @@ export default function Home() {
     <div className="home-container">
       <h1 className="heading">Hi! I'm Jovonne, a Software Engineer</h1>
       <p>check out my work</p>
-      {/* <img src="" ></img> */}
+      <div className='jc-img'>
+      <img src="jovonnecameron.png" ></img>
+      </div>
     </div>
   )
 }
