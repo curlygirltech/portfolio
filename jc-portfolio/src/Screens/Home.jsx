1c905354a0c8e854c5240458e1f2105b28b82fa5
jcameron10 <jovonnec@gmail.com>
2021-05-07 08:33:50 -0400

    add buttons

--- jc-portfolio/src/Screens/Home.jsx
+++ jc-portfolio/src/Screens/Home.jsx
@@ -4,11 +4,13 @@ import './home.css'
 export default function Home() {
   return (
     <div className="home-container">
-      <h1 className="heading">Hi! I'm Jovonne, a Software Engineer</h1>
-      <p>check out my work</p>
+      <h1 className="heading">Hi! I'm <b>Jovonne</b>, a Software Engineer</h1>
+      <p>I build full-stack apps with a front-end focus</p>
       <div className='jc-img'>
-      <img src="jovonnecameron.png" ></img>
+        <img src="jovonnecameron.png" ></img>
       </div>
+      <button>Get Resume</button>
+      <button>My Work</button>
     </div>
   )
 }
