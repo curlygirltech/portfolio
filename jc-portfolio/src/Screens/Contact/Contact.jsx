cf74f668e483f1a058cd8bb1e28a42d9fe11072e
jcameron10 <jovonnec@gmail.com>
2021-05-12 20:29:55 -0400

    contact page

--- jc-portfolio/src/Screens/Contact/Contact.jsx
+++ jc-portfolio/src/Screens/Contact/Contact.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import './contact.scss'
+import React from "react";
+import "./contact.scss";
 
 export default function Contact() {
   return (
     <div className="contact" id="contact">
-      <h1>Contact</h1>
+      {/* <div className="left"></div> */}
+      <div className="right">
+        <h2>Contact</h2>
+        <form>
+          <input type="text" placeholder="Subject" />
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
