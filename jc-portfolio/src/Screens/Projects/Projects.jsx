29c9857f796e5e8dbb2fc9d8ec5af7dc1eee0b62
jcameron10 <jovonnec@gmail.com>
2021-05-13 14:08:37 -0400

    add technologic portfolio

--- jc-portfolio/src/Screens/Projects/Projects.jsx
+++ jc-portfolio/src/Screens/Projects/Projects.jsx
@@ -5,6 +5,14 @@ export default function Projects() {
   return (
     <div className="projects" id="projects">
       <h1> Check out My Work</h1>
+      <div className="projects-container">
+        <div className="item">
+          <a href="https://heuristic-ptolemy-d4f1c6.netlify.app/">
+          <img src="technologic.png" alt="picture of website" />
+          <h3>Technologic</h3>
+          </a>
+        </div>
+      </div>
       <a href="https://drive.google.com/file/d/16o3r8-YwZWgUIaEnBMEPb3bga3LYyt7Y/view?usp=sharing">
         <h3>Download my resume</h3>
         <img src="download-symbol.png" alt="download resume" />
