99afaff13a7d2b2890a3d0a86e5802f53fc8411b
jcameron10 <jovonnec@gmail.com>
2021-05-30 12:23:46 -0400

    add sharespace

--- jc-portfolio/src/Screens/Projects/Projects.jsx
+++ jc-portfolio/src/Screens/Projects/Projects.jsx
@@ -23,8 +23,41 @@ export default function Projects() {
             MongoDB, Express, React, and NodeJS
           </p>
           <div className="project-button">
+            <a href="https://heuristic-ptolemy-d4f1c6.netlify.app/">
             <button>Live Site</button>
+            </a>
+            <a href="https://github.com/sponticel/technologic-app">
             <button>GitHub Repo</button>
+            </a>
+          </div>
+
+          <div className="projects-container">
+            <div className="item">
+              <a href="https://sharespaceapp.netlify.app/">
+              <img src="sharespace.png" alt="" />             
+              </a>
+            </div>
+            <div className="projects-about">
+            <h3>ShareSpace</h3>
+          <p>
+            SquareSpace is an inventory app inspired by Craigslist where users
+            buy and sell pre-loved tech devices. Users can create an account and
+            contact sellers or create posts for items that they wish to sell.
+          </p>
+          <p>
+            This application was built in collaboration with engineers using
+            MongoDB, Express, React, and NodeJS
+          </p>
+          <div className="project-button">
+            <a href="">
+            <button>Live Site</button>
+            </a>
+            <a href="https://github.com/sponticel/technologic-app">
+            <button>GitHub Repo</button>
+            </a>
+          </div>
+
+            </div>
           </div>
           
         </div>
