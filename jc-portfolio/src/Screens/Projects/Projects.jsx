4041aa823611896f7fb037083a1c774ce58fb61e
jcameron10 <jovonnec@gmail.com>
2021-05-30 11:42:10 -0400

    project section

--- jc-portfolio/src/Screens/Projects/Projects.jsx
+++ jc-portfolio/src/Screens/Projects/Projects.jsx
@@ -9,9 +9,24 @@ export default function Projects() {
         <div className="item">
           <a href="https://heuristic-ptolemy-d4f1c6.netlify.app/">
             <img src="technologic.png" alt="" />
-            <h3>Technologic</h3>
           </a>
-          <p>Technologic is a full stack app</p>
+        </div>
+        <div className="projects-about">
+            <h3>Technologic</h3>
+          <p>
+            Technologic is an inventory app inspired by Craigslist where users
+            buy and sell pre-loved tech devices. Users can create an account and
+            contact sellers or create posts for items that they wish to sell.
+          </p>
+          <p>
+            This application was built in collaboration with engineers using
+            MongoDB, Express, React, and NodeJS
+          </p>
+          <div className="project-button">
+            <button>Live Site</button>
+            <button>GitHub Repo</button>
+          </div>
+          
         </div>
       </div>
     </div>
