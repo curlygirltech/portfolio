14f4255d851d7f22dd65bdd4bb6ecd9a1b7f1939
jcameron10 <jovonnec@gmail.com>
2021-05-30 11:08:09 -0400

    slight changes to about section

--- jc-portfolio/src/Screens/About/About.jsx
+++ jc-portfolio/src/Screens/About/About.jsx
@@ -6,15 +6,24 @@ export default function About() {
     <div className="about-container" id="about">
       <div className="about-card">
         <h1>About Me</h1>
-        <p>Hello! I am a dynamic Software Engineer who aims to bridge the gap between functionality and creativity by approaching problems from an entrepreneurial perspective. With a background in communications and education, quick-thinking and productivity are second-skin. I love to serve companies that stimulate change with ambitious goals by using code to solve everyday problems and streamline processes. 
-
-👩🏾‍💻Tech Stack 
-
-Frontend Development: React, Vanilla JavaScript, HTML/ CSS
-
-Backend  Development: Python, PostgreSQL, MongoDB, Ruby on Rails, Express JS, Node JS.
-</p>
+        <p>
+          Hello! I am a dynamic Software Engineer who aims to bridge the gap
+          between functionality and creativity by approaching problems from an
+          entrepreneurial perspective. With a background in communications and
+          education, quick-thinking and productivity are second-skin. I love to
+          serve companies that stimulate change with ambitious goals by using
+          code to solve everyday problems and streamline processes.
+        </p>
       </div>
+        <div className="tech-stack-card">
+        <h2>👩🏾‍💻Tech Stack:</h2>
+        <h4>
+          Frontend Development: React, Vanilla JavaScript, HTML/
+          CSS, SASS
+          Backend Development: Python, PostgreSQL, MongoDB, Ruby on Rails,
+          Express JS, Node JS.
+        </h4>
+        </div>
     </div>
   );
 }
