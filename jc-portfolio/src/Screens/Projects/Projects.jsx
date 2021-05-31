43508ecf6a27f9cd0d3661d4ecd56eb18c77e993
jcameron10 <jovonnec@gmail.com>
2021-05-31 12:19:36 -0400

    created project data file

--- jc-portfolio/src/Screens/Projects/Projects.jsx
+++ jc-portfolio/src/Screens/Projects/Projects.jsx
@@ -3,63 +3,96 @@ import "./projects.scss";
 
 export default function Projects() {
   return (
-    <div className="projects" id="projects">
-      <h1> Check out My Work</h1>
-      <div className="projects-container">
-        <div className="item">
-          <a href="https://heuristic-ptolemy-d4f1c6.netlify.app/">
-            <img src="technologic.png" alt="" />
-          </a>
-        </div>
-        <div className="projects-about">
-          <h3>Technologic</h3>
-          <p>
-            Technologic is an inventory app inspired by Craigslist where users
-            buy and sell pre-loved tech devices. Users can create an account and
-            contact sellers or create posts for items that they wish to sell.
-          </p>
-          <p>
-            This application was built in collaboration with engineers using
-            MongoDB, Express, React, and NodeJS
-          </p>
-          <div className="project-button">
+    <>
+      <div className="projects" id="projects">
+        <h1> Check out My Work</h1>
+        <div className="projects-container">
+          <div className="item">
             <a href="https://heuristic-ptolemy-d4f1c6.netlify.app/">
-              <button>Live Site</button>
-            </a>
-            <a href="https://github.com/sponticel/technologic-app">
-              <button>GitHub Repo</button>
+              <img src="technologic.png" alt="" />
             </a>
           </div>
-
-          <div className="projects-container">
-            <div className="item">
-              <a href="https://sharespaceapp.netlify.app/">
-                <img src="sharespace.png" alt="" />
+          <div className="projects-about">
+            <h3>Technologic</h3>
+            <p>
+              Technologic is an inventory app inspired by Craigslist where users
+              buy and sell pre-loved tech devices. Users can create an account
+              and contact sellers or create posts for items that they wish to
+              sell.
+            </p>
+            <p>
+              This application was built in collaboration with engineers using
+              MongoDB, Express, React, and NodeJS
+            </p>
+            <div className="project-button">
+              <a href="https://heuristic-ptolemy-d4f1c6.netlify.app/">
+                <button>Live Site</button>
+              </a>
+              <a href="https://github.com/sponticel/technologic-app">
+                <button>GitHub Repo</button>
               </a>
             </div>
-            <div className="projects-about">
-              <h3>ShareSpace</h3>
-              <p>
-                Sharespace is a full stack app where guests can rent luxury
-                cabins homes in their place of choice. Users can create an
-                account, book stays, and manage their trips on their account page.
-              </p>
-              <p>
-                This application was built in collaboration with engineers using
-                MongoDB, Express, React, and NodeJS
-              </p>
-              <div className="project-button">
+
+            <div className="projects-container">
+              <div className="item">
                 <a href="https://sharespaceapp.netlify.app/">
-                  <button>Live Site</button>
+                  <img src="sharespace.png" alt="" />
                 </a>
-                <a href="https://github.com/curlygirltech/sharespace">
-                  <button>GitHub Repo</button>
-                </a>
+              </div>
+              <div className="projects-about">
+                <h3>ShareSpace</h3>
+                <p>
+                  Sharespace is a full stack app where guests can rent luxury
+                  cabins homes in their place of choice. Users can create an
+                  account, book stays, and manage their trips on their account
+                  page.
+                </p>
+                <p>
+                  This application was built using Ruby, Ruby on Rails, and
+                  React
+                </p>
+                <div className="project-button">
+                  <a href="https://sharespaceapp.netlify.app/">
+                    <button>Live Site</button>
+                  </a>
+                  <a href="https://github.com/curlygirltech/sharespace">
+                    <button>GitHub Repo</button>
+                  </a>
+
+                  <div className="projects-container">
+                    <div className="item">
+                      <a href="https://curlygirltech.github.io/Discographi/">
+                        <img src="discographi.png" alt="" />
+                      </a>
+                    </div>
+                    <div classname="projects-about">
+                      <h3>Discographi</h3>
+                      <p>
+                        dsi is a full stack app where guests can rent
+                        luxury cabins homes in their place of choice. Users can
+                        create an account, book stays, and manage their trips on
+                        their account page.
+                      </p>
+                      <p>
+                        This application was built using Ruby, Ruby on Rails,
+                        and React
+                      </p>
+                    </div>
+                    <div className="project-button">
+                      <a href="https://curlygirltech.github.io/Discographi/">
+                        <button>Live Site</button>
+                      </a>
+                      <a href="https://github.com/curlygirltech/Discographi">
+                        <button>GitHub Repo</button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ jc-portfolio/src/services/projectData.js
@@ -0,0 +1,36 @@
+export const projectData = [
+{
+  image="technologic.png",
+  title= "Technologic",
+  description = "Technologic is an inverntory app inspired by Craigslist where users buy and sell pre-loved tech devices. Users can create an account and contact sellers or create posts for items that they wish to sell.",
+  languages = "MongoDB, Express, React, Node"
+  site = "https://heuristic-ptolemy-d4f1c6.netlify.app/",
+  github= "https://github.com/sponticel/technologic-app"
+},
+{
+  image= "sharespace.png",
+  title= "ShareSpace",
+  description = "Sharespace is a full stack app where guests can rent luxury cabins homes in their place of choice. Users can create an account, book stays, and manage their trips on their account page.",
+  languages= "Ruby, Ruby on Rails, React",
+  site = "https://sharespaceapp.netlify.app/",
+  github= "https://github.com/curlygirltech/sharespace"
+},
+{
+  image= "discographi.png",
+  title= "Discographi",
+  description = "Discographi is an app that allows the user to search an artist or group to see their bio, genre and social media.",
+  languages = "HTML, CSS, Vanilla Javascript",
+  site = "https://curlygirltech.github.io/Discographi/",
+  github= "https://github.com/curlygirltech/Discographi"
+},
+{
+  image=
+  title=
+  description =
+  site =
+  github= 
+},
+
+
+
+]
